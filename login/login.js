38852d5671a3050d0d4194dd53eb96ecfafaf98d
benjmason <ben.mason0224@gmail.com>
2025-11-04 08:35:09 -0500

    Nuked it

--- login/login.js
+++ login/login.js
@@ -8,14 +8,7 @@ loginButton.addEventListener("click", (e) => {
     const username = loginForm.username.value;
     const password = loginForm.password.value;
 
-    
-
-
-
-
-
-
-    /*if (username === "user" && password === "web_dev") {
+    if (username === "user" && password === "web_dev") {
         alert("You have successfully logged in as a user.");
         window.location.href = "../user/dashboard/dashboard.html"
         //location.reload();
@@ -26,5 +19,5 @@ loginButton.addEventListener("click", (e) => {
     }
     else {
         loginErrorMsg.style.opacity = 1;
-    }*/
+    }
 })
